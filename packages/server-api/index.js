a3b72b1aa5b2c3a7dce2122e9f87e2453b6639b7
jamalmohamad <mohammed.jamalhadi@gmail.com>
2021-06-21 01:10:58 +0200

    feat: initial commit

---
+++ packages/server-api/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("I am a backend server");
+})
+
+app.listen(port, (err) => {
+    if(err) {
+        console.log(`Error: ${err.message}`);
+    } else {
+        console.log(`Listen on port ${port}`);
+    }
+})
